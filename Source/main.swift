3842a42d9d05b71604482ca9a64b0063996a3556
Anthony Ilinykh <anthonyilinykh@gmail.com>
2018-12-04 13:49:11 +0300

    Use one script to run all Katas

--- Source/main.swift
+++ Source/main.swift
@@ -11,3 +11,25 @@ for i in 0 ..< actualClassCount {
     }
 }
 */
+
+
+let curPath = FileManager.default.currentDirectoryPath
+let fullPath = NSString(string: curPath).appendingPathComponent(#file)
+let path = NSString(string: fullPath).deletingLastPathComponent
+
+if let enumerator = FileManager.default.enumerator(atPath: path) {
+    while let file = enumerator.nextObject() as? String {
+
+        if file == "main.swift" { continue }
+
+        let task = Process()
+        task.launchPath = "/usr/bin/env"
+        task.arguments = ["swift", "-F", "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks", NSString(string: path).appendingPathComponent(file)]
+        task.launch()
+        task.waitUntilExit()
+    }
+}
+else {
+    print("Can't get enumerator at path \(path)")
+}
+
